c5bc3513222afd3927b3681cdc2ae1b101676fd3
cdzhangxiaolong6 <cdzhangxiaolong6@jd.com>
2020-04-01 17:10:32 +0800

    两数之和

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main main = new Main();
+
+        int[] nums = {0,2,4,5,9};
+        int target = 9;
+        main.twoSum(nums, target);
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[j] == target - nums[i]) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        throw new IllegalArgumentException("No two sum solution");
+    }
+}
